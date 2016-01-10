e11cdca334ef1e10e8b11d0e7809a451a5da2e04
Jason Tam <jzntam@gmail.com>
2016-01-10 01:37:01 -0800

    Refactor weatherAPI to seperate service

--- controllers.js
+++ controllers.js
@@ -9,27 +9,11 @@ weatherApp.controller('homeController', ['$scope', '$location', 'weatherService'
   };
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'weatherService', function ($scope, $resource, $routeParams, weatherService) {
+weatherApp.controller('forecastController', ['$scope', '$routeParams', 'weatherService', 'weatherApiService', function ($scope,  $routeParams, weatherService, weatherApiService) {
   $scope.city = weatherService.city;
   $scope.days = $routeParams.days || '2';
 
-  $scope.weatherAPI = $resource(
-    "http://api.openweathermap.org/data/2.5/forecast/daily",
-    {
-      callback: "JSON_CALLBACK"
-    },
-    {
-      get: { method: "JSONP" }
-    }
-  );
-
-  $scope.weatherResult = $scope.weatherAPI.get(
-    {
-      q:     $scope.city,
-      cnt:   $scope.days,
-      appid: '92dee419b1737be748405422b0f195dd'
-    }
-  );
+  $scope.weatherResult = weatherApiService.GetWeather($scope.city, $scope.days)
 
   $scope.convertToFahrenheit = function(degK) {
     return Math.round((1.8 * (degK - 273.15)) + 32);
--- services.js
+++ services.js
@@ -1,3 +1,19 @@
 weatherApp.service('weatherService', function(){
   this.city = "New York, NY"
 })
+
+weatherApp.service('weatherApiService', ['$resource', function($resource){
+  this.GetWeather =  function(city, days){
+    var weatherAPI = $resource(
+      "http://api.openweathermap.org/data/2.5/forecast/daily",
+      {
+        callback: "JSON_CALLBACK"
+      },
+      {
+        get: { method: "JSONP" }
+      }
+    );
+
+    return weatherAPI.get({q: city, cnt: days, appid: '92dee419b1737be748405422b0f195dd'});
+  }
+}])
